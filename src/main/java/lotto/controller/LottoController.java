3f87025b1fd143bf5bc9f892ce842a8736616a69
HuitaePark <qkrgmlxo3174@gmail.com>
2025-09-16 12:33:18 +0900

    refactor: OutputView 클래스 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
 import lotto.domain.LottoCashier;
+import lotto.domain.LottoMachine;
+import lotto.domain.Lottos;
 import lotto.domain.PurchaseAmount;
 import lotto.ui.InputHandler;
 import lotto.ui.OutputView;
@@ -8,22 +10,23 @@ import lotto.ui.OutputView;
 public class LottoController {
 
     private final InputHandler inputHandler;
+    private final OutputView outputView;
 
-    public LottoController(InputHandler inputHandler) {
+    public LottoController(InputHandler inputHandler, OutputView outputView) {
         this.inputHandler = inputHandler;
+        this.outputView = outputView;
     }
 
     public void startLottery(){
         String input = inputHandler.inputText();
         PurchaseAmount purchaseAmount = new PurchaseAmount(input);
-        giveLottoCashier(purchaseAmount);
+
+        giveToLottoCashier(purchaseAmount);
     }
-    private void giveLottoCashier(PurchaseAmount purchaseAmount){
-        LottoCashier lottoCashier = new LottoCashier(purchaseAmount);
-        int lottoCount = lottoCashier.calculateLottoCount();
-        reportLottoCount(lottoCount);
+    private void giveToLottoCashier(LottoMachine lottoMachine,PurchaseAmount purchaseAmount){
+        LottoCashier lottoCashier = new LottoCashier(lottoMachine,purchaseAmount);
     }
     private void reportLottoCount(int lottoCount){
-        OutputView.printLottoCount(lottoCount);
+        outputView.printLottoCount(lottoCount);
     }
 }
--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -4,11 +4,11 @@ import lotto.global.constant.LottoInfoMessage;
 
 public class OutputView {
 
-    public static void printStartMessage(){
+    public void printStartMessage(){
         System.out.println(LottoInfoMessage.LOTTO_START_MESSAGE);
     }
 
-    public static void printLottoCount(int lottoCount) {
+    public void printLottoCount(int lottoCount) {
         System.out.println(LottoInfoMessage.LOTTO_PURCHASED_MESSAGE.getMessage(lottoCount));
     }
 
